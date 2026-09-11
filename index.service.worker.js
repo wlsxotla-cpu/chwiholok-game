8aaf746ecf5ca9d7aca380a9bfc2cfdeb5d586fc
심진태 <wlsxotla@gmail.com>
2026-09-11 15:29:49 +0900

    Deploy locked-character weapon fix to production and /dev/

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789106643|8912566';
+const CACHE_VERSION = '1789108151|9177028';
 /** @type {string} */
 const CACHE_PREFIX = '취호록-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
