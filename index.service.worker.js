60990321b78616b67caef18af9671e7017eb8910
심진태 <wlsxotla@gmail.com>
2026-09-12 14:47:27 +0900

    Deploy v0.44.0 to production and /dev/ (UI density fixes, nickname-once onboarding, fusion combo indicator)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789190759|8892045';
+const CACHE_VERSION = '1789192001|9256041';
 /** @type {string} */
 const CACHE_PREFIX = '취호록-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
