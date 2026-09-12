d8359656053ba0934803b48f8c1a3bbbfac5ba93
심진태 <wlsxotla@gmail.com>
2026-09-12 14:06:02 +0900

    Deploy live Firebase-backed ranking system to production and /dev/

--- dev/index.service.worker.js
+++ dev/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789180014|9185912';
+const CACHE_VERSION = '1789189519|9194062';
 /** @type {string} */
 const CACHE_PREFIX = '취호록-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
