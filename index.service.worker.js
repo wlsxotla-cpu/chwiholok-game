574fd4dc96321cf674a486aed716a06c6f9dcf1d
심진태 <wlsxotla@gmail.com>
2026-09-12 11:27:35 +0900

    Deploy v0.42.0 to production and /dev/ (ranking tab, inactive until Firebase keys set)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789177615|8818088';
+const CACHE_VERSION = '1789180014|9185912';
 /** @type {string} */
 const CACHE_PREFIX = '취호록-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
