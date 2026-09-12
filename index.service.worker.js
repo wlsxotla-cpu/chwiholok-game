09967d9e077b35bb145fd315ee4107c146d74a7d
심진태 <wlsxotla@gmail.com>
2026-09-12 10:25:06 +0900

    Deploy glow-disable to production and /dev/ (mobile heat mitigation)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789109556|8972353';
+const CACHE_VERSION = '1789176267|10099294';
 /** @type {string} */
 const CACHE_PREFIX = '취호록-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
